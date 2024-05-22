4b88e71b1f0b30fbb704ff3dd008066b812a06cb
FiddllePat <89714380+FiddllePat@users.noreply.github.com>
2024-05-23 09:35:29 +1000

    Create xor_gate.py

---
+++ xor_gate.py
@@ -0,0 +1,29 @@
+import machine
+import neopixel
+import time
+
+pixel_pin = 16
+pixel = neopixel.NeoPixel(machine.Pin(pixel_pin), 1)
+
+input_pins = [machine.Pin(i, machine.Pin.IN) for i in range(16)] + [machine.Pin(i, machine.Pin.IN) for i in range(26, 30)]
+
+def count_high_pins(pins):
+    return sum(pin.value() for pin in pins)
+
+while True:
+    if count_high_pins(input_pins) % 2 == 1:
+        pixel[0] = (0, 255, 0)
+        pixel.write()
+        time.sleep(1)
+
+        pixel[0] = (0, 0, 255)
+        pixel.write()
+        time.sleep(1)
+        
+        pixel[0] = (0, 255, 0)
+        pixel.write()
+        time.sleep(1)
+    else:
+        pixel[0] = (0, 0, 0)
+        pixel.write()
+        time.sleep(0.1)
