b714a111de0813a2052d2db9cbefa526b0c57de1
FiddllePat <89714380+FiddllePat@users.noreply.github.com>
2024-06-03 16:38:23 +1000

    Update or_gate.py

--- or_gate.py
+++ or_gate.py
@@ -2,27 +2,22 @@ import machine
 import neopixel
 import time
 
+# Pin for the NeoPixel
 pixel_pin = 16
 pixel = neopixel.NeoPixel(machine.Pin(pixel_pin), 1)
-input_pins = [machine.Pin(i, machine.Pin.IN) for i in range(16)] + [machine.Pin(i, machine.Pin.IN) for i in range(26, 30)]
+
+# Define pins 8-14 as input pins
+input_pins = [machine.Pin(i, machine.Pin.IN) for i in range(8, 15)]
 
 def any_pin_high(pins):
     return any(pin.value() for pin in pins)
 
 while True:
     if any_pin_high(input_pins):
-        pixel[0] = (0, 255, 0)  # Green
-        pixel.write()
-        time.sleep(1)
-
-        pixel[0] = (0, 0, 255)  # Blue
-        pixel.write()
-        time.sleep(1)
-        
-        pixel[0] = (0, 255, 0)  # Green
+        pixel[0] = (84, 0, 159)
         pixel.write()
         time.sleep(1)
     else:
-        pixel[0] = (0, 0, 0)  # Turn off the LED if no inputs are high
+        pixel[0] = (0, 0, 0) # turn off the pin
         pixel.write()
-        time.sleep(0.1)  # Wait for a short time before checking again
+        time.sleep(0.1) 
