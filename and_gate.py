02eb0e3b76ed5be040bd89a826262c4d3083f4a3
FiddllePat <89714380+FiddllePat@users.noreply.github.com>
2024-06-03 16:31:16 +1000

    Update and_gate.py

--- and_gate.py
+++ and_gate.py
@@ -2,21 +2,22 @@ import machine
 import neopixel
 import time
 
-pixel_pin = 16
+pixel_pin = 16 # pixel pin
 pixel = neopixel.NeoPixel(machine.Pin(pixel_pin), 1)
 
-input_pins = [machine.Pin(i, machine.Pin.IN) for i in range(16)] + [machine.Pin(i, machine.Pin.IN) for i in range(26, 30)]
+
+input_pins = [machine.Pin(i, machine.Pin.IN) for i in range(8, 15)] # pin array define
 
 def count_high_pins(pins):
     return sum(pin.value() for pin in pins)
 
 while True:
-    if count_high_pins(input_pins) >= 2:
+     if count_high_pins(input_pins) >= 7: # if all the pins are high then light it up
         pixel[0] = (84, 0, 159)
         pixel.write()
         time.sleep(1)
     else:
-        pixel[0] = (0, 0, 0)
+        pixel[0] = (0, 0, 0) if # if all pins are not high then dont light up
         pixel.write()
         time.sleep(0.1)
 
